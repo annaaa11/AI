b426d6cdd4f55fde6abb0796eaceb08924ea3632
annaaa11 <gosberex@gmail.com>
2025-06-11 17:34:51 +0300

    # Прочитайте # файл data/lesson9/return_policy.txt # та # напишіть простий чат для відповідей на питання користувачів # стосовно повернення товару.

--- test.py
+++ test.py
@@ -1,113 +1,56 @@
-#  Завдання 1
-# Відкрийте відео data/lesson_pose/squat.mp4
-# Ваша задача рахувати кількість присідань.
-# Отримайте перший кадр та виділіть основні точки.
-# Отримайте координати 3-ох точок ноги
-# Визначте кут між цими трьома точками. Скористайтесь
-# функцією utils.get_angle(x1, y1, x2, y2, x3, y3) де x2, y2 –
-# координати коліна(центральна точка)
-# Запустіть відео та добавте на сам кадр кут згинання ніг.
-# Визначіть нижню межу кута(якщо людина опустилась
-# нижче  вважаємо що вона достатньо опустилась) та верхню
-# межу кута(якщо людина піднялась вище вважаємо що вона
-# достатньо піднялась)
-# Добавте кількість присідань та
-# кут на кожен кадр.
+# Прочитайте
+# файл data/lesson9/return_policy.txt
+# та
+# напишіть простий чат для відповідей на питання користувачів
+# стосовно повернення товару.
+# Діалог завершується коли користувач ввів порожній
+# рядок.
+# Модель отримує інструкцію з вмістом файлу та всю
+# історію спілкування разом з новим повідомленням у форматі
+# Instruction: …
+# Human: message1
+# AI: message2
+# Human: message3
+# AI: message4
+# Human: message5
+# AI:
 
-import cv2
-import numpy as np
-import ultralytics
+from langchain_google_genai import GoogleGenerativeAI
 
-#Визначте кут між цими трьома точками
-def get_angle(x1, y1, x2, y2, x3, y3):
-    a = np.array([x1, y1])
-    b = np.array([x2, y2])
-    c = np.array([x3, y3])
+import dotenv
+import os
 
-    ab = a - b
-    cb = c - b
+# завантажити api ключі з папки .env
+dotenv.load_dotenv()
 
-    dot = ab @ cb
-    norm_ab = (ab @ ab) ** 0.5
-    norm_cb = (cb @ cb) ** 0.5
-    angle = np.arccos(dot / norm_ab / norm_cb)
-    angle = angle / np.pi * 180
+# отримати сам ключ
+api_key = os.getenv('GEMINI_API_KEY')
 
-    return angle
+with open("data/lesson9/return_policy.txt", "r", encoding="UTF-8") as file:
+    rulse = file.read()
 
-model = ultralytics.YOLO('yolo11n-pose.pt')
-cap = cv2.VideoCapture('data/lesson_pose/squat.mp4')
+llm = GoogleGenerativeAI(
+    model='gemini-2.0-flash',  # назва моделі
+    google_api_key=api_key,    # ваша API
+    #top_k=10,                  # серед скількох найбільш ймовірних слів обирати нове слово
+    temperature=0.3,            # температура
+    #max_output_tokens=10        # максимальна довжина відповіді у токенах
+)
 
-squat_count = 0
-is_down = False
-
-LOWER_ANGLE = 75   # присідання виконано
-UPPER_ANGLE = 155  # піднявся
+query = f'''Instruction: Ти - чат бот. Твоя задача - дай відповіді щодо правил повернення
+товару {rulse} користувачеві на основі його питань в віччливій формі.'''
 
 while True:
-    ret, frame = cap.read()
-    if not ret:
+    question = input('Ваше питання стосовно повернення товару або Ентер для закінчення. ')
+
+    if question == '':
         break
 
-    frame = cv2.resize(frame, None, fx=0.3, fy=0.3)
-    results = model.predict(frame)
-    res1 = results[0]
 
-    xy = res1.keypoints.xy.numpy()[0]
-    xy = xy.astype(int)
+    query += f'\n HUMAN: {question}\n AI:'
 
-    # Точки: таз (hip) = 12, коліно = 14, щиколотка = 16 (для правої ноги)
-    x1, y1 = xy[12]
-    x2, y2 = xy[14]
-    x3, y3 = xy[16]
+    response = llm.invoke(query)
+    query += response
 
-    angle = int(get_angle(x1, y1, x2, y2, x3, y3))
-
-    # Малюємо ключові точки
-    for (x, y) in [ (x1, y1), (x2, y2), (x3, y3) ]:
-        frame = cv2.circle(
-            frame,  # зображення на якому намалювати коло
-            (x, y),  # координати центру кола
-            5,  # радіус у пікселях
-            (0, 255, 0),  # колір у bgr форматі(тут -- зелений)
-            -1  # товщина лінії(-1 -- запоанити коло повністю)
-        )
-
-    # Підрахунок присідань
-    if angle < LOWER_ANGLE and not is_down:
-        is_down = True
-    elif angle > UPPER_ANGLE and is_down:
-        squat_count += 1
-        is_down = False
-
-    # Виводимо кут та кількість присідань
-
-    frame = cv2.putText(
-        frame,  # зображення на яке насти текст
-        f'Angle: {angle}',  # сам текст
-        (50, 100),  # координати тексту
-        cv2.FONT_HERSHEY_SIMPLEX,  # шрифт
-        1.5,  # розмір шрифту
-        (255, 255, 255),  # колір у форматі bgr(тут -- білий)
-        2  # товщина лінії
-    )
-
-    frame = cv2.putText(
-        frame,  # зображення на яке насти текст
-        f'Squats: {squat_count}',  # сам текст
-        (50, 50),  # координати тексту
-        cv2.FONT_HERSHEY_SIMPLEX,  # шрифт
-        1.5,  # розмір шрифту
-        (255, 255, 255),  # колір у форматі bgr(тут -- білий)
-        2  # товщина лінії
-    )
-
-
-    cv2.imshow('Squat Counter', frame)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+    print(response)
 
