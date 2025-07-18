74b91d9ceb473f82d72f374792255fce1e705b87
annaaa11 <gosberex@gmail.com>
2025-07-18 13:15:42 +0300

    # Розумний пошук у Твіттері з використанням ШІ

---
+++ teste.py
@@ -0,0 +1,19 @@
+import requests
+import pinecone
+from pinecone import Pinecone, ServerlessSpec
+from sentence_transformers import SentenceTransformer
+import spacy
+from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from datetime import datetime, timedelta
+import streamlit as st
+import re
+import logging
+import os
+
+
+
+nlp = spacy.load("en_core_web_sm")
