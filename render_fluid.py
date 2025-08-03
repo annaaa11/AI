64268881dfb9072f7b68a26bdbb16590853a0cdd
annaaa11 <gosberex@gmail.com>
2025-08-03 18:46:17 +0300

    fluid

--- render_fluid.py
+++ render_fluid.py
@@ -4,10 +4,10 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
 import time
 import re
 from datetime import datetime
+
 
 # Конфигурация
 BOT_TOKEN = "7652720412:AAFkPwpqFa3iRr23xw8rE9MYXtj_ptvq6kk"
