226a5df685f1f41f41ef25dbf25fa9b9ce202c65
annaaa11 <gosberex@gmail.com>
2025-08-03 17:58:49 +0300

    aave

--- aave.py
+++ aave.py
@@ -117,17 +117,30 @@ def get_transactions():
         return resp["result"]
     return []
 
+CHAT_IDS = [6192278046, 306507209]  # список id пользователей
 
 def send_to_telegram(message):
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    payload = {
-        "chat_id": CHAT_ID,
-        "text": message
-    }
-    try:
-        requests.post(url, data=payload)
-    except Exception as e:
-        print(f"Ошибка отправки в Telegram: {e}")
+    url_base = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    for chat_id in CHAT_IDS:
+        payload = {
+            "chat_id": chat_id,
+            "text": message
+        }
+        try:
+            requests.post(url_base, data=payload)
+        except Exception as e:
+            print(f"Ошибка при отправке в чат {chat_id}: {e}")
+
+# def send_to_telegram(message):
+#     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+#     payload = {
+#         "chat_id": CHAT_ID,
+#         "text": message
+#     }
+#     try:
+#         requests.post(url, data=payload)
+#     except Exception as e:
+#         print(f"Ошибка отправки в Telegram: {e}")
 
 def process_transactions():
     seen_hashes = set()
@@ -146,7 +159,7 @@ def process_transactions():
 
                 tx_time = datetime.fromtimestamp(int(tx["timeStamp"]), tz=timezone.utc)
                 # Фильтр по времени - только за последний час
-                if (now - tx_time).total_seconds() > 300:
+                if (now - tx_time).total_seconds() > 2000:
                     continue
 
                 # Формируем сообщение
