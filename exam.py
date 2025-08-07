c5ad55d7e68e95819997633b5e902ebbe397308c
annaaa11 <gosberex@gmail.com>
2025-08-07 17:30:11 +0300

    Update exam.py

--- exam.py
+++ exam.py
@@ -43,7 +43,7 @@ dotenv.load_dotenv()
 # API keys
 api_key = os.getenv("GEMINI_API_KEY")
 pinecone_key = os.getenv("PINECONE_API_KEY")
-twitter_api_key = os.getenv("TWITTER_API_KEY") or "b45c33e1de7d49c2a761857d7ac9ec01"
+twitter_api_key = os.getenv("TWITTER_API_KEY") or "d877c50a12614b03bc68191e2b44f3ea" 
 
 # Validate API keys
 if not api_key:
