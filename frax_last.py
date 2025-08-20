e8a1e9004814edc8ad93281938246853910b631f
annaaa11 <gosberex@gmail.com>
2025-08-20 13:40:39 +0300

    frax 1 час

--- frax_last.py
+++ frax_last.py
@@ -218,11 +218,11 @@ def fetch_fraxlend_v1_frax_fxs_rate(driver):
     try:
         url = "https://app.frax.finance/staking/overview"
         # Set up Selenium WebDriver
-        options = webdriver.ChromeOptions()
-        options.add_argument('--headless')
-        options.add_argument(
-            '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36')
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+        #options = webdriver.ChromeOptions()
+        #options.add_argument('--headless')
+       # options.add_argument(
+      #      '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36')
+       # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
         # Load the page
         driver.get(url)
@@ -230,7 +230,7 @@ def fetch_fraxlend_v1_frax_fxs_rate(driver):
 
         # Parse page source
         soup = BeautifulSoup(driver.page_source, 'html.parser')
-        driver.quit()
+        #driver.quit()
 
         # Search for target text
         target_text = re.compile(r"Fraxlend\s*V1\s*FRAX/FXS", re.IGNORECASE)
