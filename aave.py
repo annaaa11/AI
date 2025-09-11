23ccd7e6097be20ea0a84d9026576aa8f6ef330f
annaaa11 <gosberex@gmail.com>
2025-09-11 14:16:04 +0300

    3 aave token parsing

--- aave.py
+++ aave.py
@@ -85,6 +85,222 @@
 #     main_loop()
 
 
+# import requests
+# import time
+# import threading
+# from datetime import datetime, timezone
+# from flask import Flask
+# from web3 import Web3
+# import json
+#
+# API_KEY = "4ZNE8XMUQMTKWCWB6N112S2KI3A4TF2DBI"
+# ADDRESS = "0x4579a27af00a62c0eb156349f31b345c08386419".lower()
+# BOT_TOKEN = "7652720412:AAFkPwpqFa3iRr23xw8rE9MYXtj_ptvq6kk"
+# CHAT_ID = "6192278046"  # ID пользователя или канала
+# CHECK_INTERVAL = 15  # секунд между проверками
+#
+# app = Flask(__name__)
+#
+# def get_transactions():
+#     url = (
+#         f"https://api.etherscan.io/api"
+#         f"?module=account"
+#         f"&action=tokentx"
+#         f"&address={ADDRESS}"
+#         f"&startblock=0"
+#         f"&endblock=99999999"
+#         f"&sort=desc"
+#         f"&apikey={API_KEY}"
+#     )
+#     resp = requests.get(url).json()
+#     if resp["status"] == "1":
+#         return resp["result"]
+#     return []
+#
+#
+# def send_to_telegram(message):
+#     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+#     payload = {
+#         "chat_id": CHAT_ID,
+#         "text": message
+#     }
+#     try:
+#         requests.post(url, data=payload)
+#     except Exception as e:
+#         print(f"Ошибка отправки в Telegram: {e}")
+#
+# def process_transactions():
+#     seen_hashes = set()
+#     while True:
+#         try:
+#             txs = get_transactions()
+#             now = datetime.now(timezone.utc)
+#             for tx in txs[:20]:
+#                 # Фильтр по полю From
+#                 if tx["from"].lower() != ADDRESS.lower():
+#                     continue
+#
+#                 tx_hash = tx["hash"]
+#                 if tx_hash in seen_hashes:
+#                     continue
+#
+#                 tx_time = datetime.fromtimestamp(int(tx["timeStamp"]), tz=timezone.utc)
+#                 # Фильтр по времени - только за последний час
+#                 if (now - tx_time).total_seconds() > 300:
+#                     continue
+#
+#                 # Формируем сообщение
+#                 token_name = tx.get("tokenName", "Unknown token")
+#                 token_decimal = int(tx.get("tokenDecimal", 18))
+#                 value = int(tx["value"]) / (10 ** token_decimal)
+#
+#                 message = (
+#                     f"Tx Hash:         {tx_hash}\n\n"
+#                     f"Timestamp (UTC): {tx_time.strftime('%Y-%m-%d %H:%M:%S')} + 3 часа\n\n"
+#                     f"Token Name:      {token_name}\n"
+#                     f"Value:           {value}"
+#                 )
+#                 print(f"Отправляю в Telegram: {tx_hash}")
+#                 send_to_telegram(message)
+#                 seen_hashes.add(tx_hash)
+#
+#         except Exception as e:
+#             print(f"Ошибка обработки транзакций: {e}")
+#
+#         time.sleep(CHECK_INTERVAL)
+#
+# @app.route("/")
+# def home():
+#     return "Parser is running!"
+#
+# @app.route("/status")
+# def status():
+#     return {"status": "ok"}
+#
+# def run_background():
+#     thread = threading.Thread(target=process_transactions)
+#     thread.daemon = True
+#     thread.start()
+#
+# if __name__ == "__main__":
+#     run_background()
+#     app.run(host="0.0.0.0", port=10000)
+
+
+# import requests
+# import time
+# import threading
+# from datetime import datetime, timezone
+# from flask import Flask
+# from web3 import Web3
+# import json
+#
+# API_KEY = "4ZNE8XMUQMTKWCWB6N112S2KI3A4TF2DBI"
+# ADDRESS = "0x4579a27af00a62c0eb156349f31b345c08386419".lower()
+# BOT_TOKEN = "7652720412:AAFkPwpqFa3iRr23xw8rE9MYXtj_ptvq6kk"
+# CHAT_ID = "6192278046"  # ID пользователя или канала
+# CHECK_INTERVAL = 15  # секунд между проверками
+#
+# app = Flask(__name__)
+#
+# def get_transactions():
+#     url = (
+#         f"https://api.etherscan.io/api"
+#         f"?module=account"
+#         f"&action=tokentx"
+#         f"&address={ADDRESS}"
+#         f"&startblock=0"
+#         f"&endblock=99999999"
+#         f"&sort=desc"
+#         f"&apikey={API_KEY}"
+#     )
+#     resp = requests.get(url).json()
+#     if resp["status"] == "1":
+#         return resp["result"]
+#     return []
+#
+# CHAT_IDS = [6192278046, 306507209]  # список id пользователей
+#
+# def send_to_telegram(message):
+#     url_base = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+#     for chat_id in CHAT_IDS:
+#         payload = {
+#             "chat_id": chat_id,
+#             "text": message
+#         }
+#         try:
+#             requests.post(url_base, data=payload)
+#         except Exception as e:
+#             print(f"Ошибка при отправке в чат {chat_id}: {e}")
+#
+# # def send_to_telegram(message):
+# #     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+# #     payload = {
+# #         "chat_id": CHAT_ID,
+# #         "text": message
+# #     }
+# #     try:
+# #         requests.post(url, data=payload)
+# #     except Exception as e:
+# #         print(f"Ошибка отправки в Telegram: {e}")
+#
+# def process_transactions():
+#     seen_hashes = set()
+#     while True:
+#         try:
+#             txs = get_transactions()
+#             now = datetime.now(timezone.utc)
+#             for tx in txs[:20]:
+#                 # Фильтр по полю From
+#                 if tx["from"].lower() != ADDRESS.lower():
+#                     continue
+#
+#                 tx_hash = tx["hash"]
+#                 if tx_hash in seen_hashes:
+#                     continue
+#
+#                 tx_time = datetime.fromtimestamp(int(tx["timeStamp"]), tz=timezone.utc)
+#                 # Фильтр по времени - только за последний час
+#                 if (now - tx_time).total_seconds() > 2000:
+#                     continue
+#
+#                 # Формируем сообщение
+#                 token_name = tx.get("tokenName", "Unknown token")
+#                 token_decimal = int(tx.get("tokenDecimal", 18))
+#                 value = int(tx["value"]) / (10 ** token_decimal)
+#
+#                 message = (
+#                     f"Tx Hash:         {tx_hash}\n\n"
+#                     f"Timestamp (UTC): {tx_time.strftime('%Y-%m-%d %H:%M:%S')} + 3 часа\n\n"
+#                     f"Token Name:      {token_name}\n"
+#                     f"Value:           {value}"
+#                 )
+#                 print(f"Отправляю в Telegram: {tx_hash}")
+#                 send_to_telegram(message)
+#                 seen_hashes.add(tx_hash)
+#
+#         except Exception as e:
+#             print(f"Ошибка обработки транзакций: {e}")
+#
+#         time.sleep(CHECK_INTERVAL)
+#
+# @app.route("/")
+# def home():
+#     return "Parser is running!"
+#
+# @app.route("/status")
+# def status():
+#     return {"status": "ok"}
+#
+# def run_background():
+#     thread = threading.Thread(target=process_transactions)
+#     thread.daemon = True
+#     thread.start()
+#
+# if __name__ == "__main__":
+#     run_background()
+#     app.run(host="0.0.0.0", port=10000)
+
 import requests
 import time
 import threading
@@ -94,19 +310,23 @@ from web3 import Web3
 import json
 
 API_KEY = "4ZNE8XMUQMTKWCWB6N112S2KI3A4TF2DBI"
-ADDRESS = "0x4579a27af00a62c0eb156349f31b345c08386419".lower()
+ADDRESSES = [
+    "0x38c503a438185cde29b5cf4dc1442fd6f074f1cc",
+    "0x285866acb0d60105b4ed350a463361c2d9afa0e2",
+    "0x38a5357ce55c81add62abc84fb32981e2626adef",
+]
 BOT_TOKEN = "7652720412:AAFkPwpqFa3iRr23xw8rE9MYXtj_ptvq6kk"
 CHAT_ID = "6192278046"  # ID пользователя или канала
 CHECK_INTERVAL = 15  # секунд между проверками
 
 app = Flask(__name__)
 
-def get_transactions():
+def get_transactions(address):
     url = (
         f"https://api.etherscan.io/api"
         f"?module=account"
         f"&action=tokentx"
-        f"&address={ADDRESS}"
+        f"&address={address}"
         f"&startblock=0"
         f"&endblock=99999999"
         f"&sort=desc"
@@ -117,52 +337,67 @@ def get_transactions():
         return resp["result"]
     return []
 
+CHAT_IDS = [6192278046, 306507209]  # список id пользователей
 
 def send_to_telegram(message):
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    payload = {
-        "chat_id": CHAT_ID,
-        "text": message
-    }
-    try:
-        requests.post(url, data=payload)
-    except Exception as e:
-        print(f"Ошибка отправки в Telegram: {e}")
+    url_base = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    for chat_id in CHAT_IDS:
+        payload = {
+            "chat_id": chat_id,
+            "text": message
+        }
+        try:
+            requests.post(url_base, data=payload)
+        except Exception as e:
+            print(f"Ошибка при отправке в чат {chat_id}: {e}")
+
+# def send_to_telegram(message):
+#     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+#     payload = {
+#         "chat_id": CHAT_ID,
+#         "text": message
+#     }
+#     try:
+#         requests.post(url, data=payload)
+#     except Exception as e:
+#         print(f"Ошибка отправки в Telegram: {e}")
 
 def process_transactions():
     seen_hashes = set()
     while True:
         try:
-            txs = get_transactions()
             now = datetime.now(timezone.utc)
-            for tx in txs[:20]:
-                # Фильтр по полю From
-                if tx["from"].lower() != ADDRESS.lower():
-                    continue
+            for address in ADDRESSES:
+                txs = get_transactions(address)
+                for tx in txs[:20]:
+                    # Фильтр по полю From
+                    if tx["from"].lower() != address:
+                        continue
 
-                tx_hash = tx["hash"]
-                if tx_hash in seen_hashes:
-                    continue
+                    tx_hash = tx["hash"]
+                    if tx_hash in seen_hashes:
+                        continue
 
-                tx_time = datetime.fromtimestamp(int(tx["timeStamp"]), tz=timezone.utc)
-                # Фильтр по времени - только за последний час
-                if (now - tx_time).total_seconds() > 300:
-                    continue
+                    tx_time = datetime.fromtimestamp(int(tx["timeStamp"]), tz=timezone.utc)
+                    # Фильтр по времени - только за последний час
+                    if (now - tx_time).total_seconds() > 2000:
+                        continue
 
-                # Формируем сообщение
-                token_name = tx.get("tokenName", "Unknown token")
-                token_decimal = int(tx.get("tokenDecimal", 18))
-                value = int(tx["value"]) / (10 ** token_decimal)
+                    # Формируем сообщение
+                    token_name = tx.get("tokenName", "Unknown token")
+                    token_decimal = int(tx.get("tokenDecimal", 18))
+                    value = int(tx["value"]) / (10 ** token_decimal)
 
-                message = (
-                    f"Tx Hash:         {tx_hash}\n\n"
-                    f"Timestamp (UTC): {tx_time.strftime('%Y-%m-%d %H:%M:%S')} + 3 часа\n\n"
-                    f"Token Name:      {token_name}\n"
-                    f"Value:           {value}"
-                )
-                print(f"Отправляю в Telegram: {tx_hash}")
-                send_to_telegram(message)
-                seen_hashes.add(tx_hash)
+                    message = (
+                        f"Address: {address}\n"
+                        f"Tx Hash:         {tx_hash}\n\n"
+                        f"Timestamp (UTC): {tx_time.strftime('%Y-%m-%d %H:%M:%S')} + 3 часа\n\n"
+                        f"Token Name:      {token_name}\n"
+                        f"Value:           {value}"
+                    )
+                    print(f"Отправляю в Telegram: {tx_hash}")
+                    send_to_telegram(message)
+                    seen_hashes.add(tx_hash)
 
         except Exception as e:
             print(f"Ошибка обработки транзакций: {e}")
